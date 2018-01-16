79c1ae336496e9bc4509d85d9ca385bf7330a79f
Emir Günim <emir.gunim@gmail.com>
2018-01-17 00:06:46 +0300

    Add basic features
    Two basic functions; Encode() and Decode() added. Initial content added to README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cp857
-Code page 857
+# Cp857
+
+Go package cp857 provides [Code page 857](https://en.wikipedia.org/wiki/Code_page_857)
+ character encodings. Package [golang.org/x/text/encoding/charmap](https://godoc.org/golang.org/x/text/encoding/charmap) has 
+ several similar character encodings; however, Code page 857 is missing.
---
+++ cp857_test.go
@@ -0,0 +1,52 @@
+package cp857_test
+
+import (
+	"testing"
+
+	"github.com/gunim/cp857"
+)
+
+func TestDecode(t *testing.T) {
+	tests := []struct {
+		name string
+		x    byte
+		want rune
+	}{
+		{"Ğ", 166, 0x011E},
+		{"ğ", 167, 0x011F},
+		{"╦", 203, 0x2566},
+		{"¶", 244, 0x00B6},
+		{"d", 100, 100},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cp857.Decode(tt.x); got != tt.want {
+				t.Errorf("Decode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEncode(t *testing.T) {
+	tests := []struct {
+		name   string
+		r      rune
+		wantB  byte
+		wantOk bool
+	}{
+		{"Ğ", 0x011E, 166, true},
+		{"ğ", 0x011F, 167, true},
+		{"╦", 0x2566, 203, true},
+		{"¶", 0x00B6, 244, true},
+		{"d", 100, 100, true},
+		{"⌘", '\u2318', '\x1A', false},
+		{"😀", '\U0001F600', '\x1A', false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotB, gotOk := cp857.Encode(tt.r); gotB != tt.wantB || gotOk != tt.wantOk {
+				t.Errorf("Encode() = %v, %v; want %v, %v", gotB, gotOk, tt.wantB, tt.wantOk)
+			}
+		})
+	}
+}
